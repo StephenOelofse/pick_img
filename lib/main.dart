66a4610135c4a2b34a617a640989521ff727a162
StephenOelofse <oelofse.stephen@gmail.com>
2022-03-20 16:37:56 +1100

    added image picker, buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const App());
@@ -29,16 +34,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  File? image;
+
+  Future pickImage(ImageSource imagesrc) async {
+    try {
+      final image = await ImagePicker().pickImage(source: imagesrc);
+      if (image == null) return;
+
+      final imageTemporary = File(image.path);
+      setState(() => this.image = imageTemporary);
+    } on PlatformException catch (e) {
+      print('failed to pick an image: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            image != null
+                ? ClipOval(
+                    child: Image.file(
+                      image!,
+                      width: 160,
+                      height: 160,
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                : const FlutterLogo(size: 130),
+            const SizedBox(
+              height: 25,
+            ),
+            Text(
+              'Image Picker',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            buttonBuild(
+                context,
+                'Pick Galary',
+                const Icon(Icons.image_outlined),
+                () => pickImage(ImageSource.gallery)),
+            const SizedBox(
+              height: 20,
+            ),
+            buttonBuild(
+                context,
+                'Pick Camera',
+                const Icon(Icons.camera_alt_outlined),
+                () => pickImage(ImageSource.camera))
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
+}
+
+@override
+Widget buttonBuild(BuildContext context, title, icon, onPressed) {
+  Size screenSize = MediaQuery.of(context).size;
+
+  return ElevatedButton.icon(
+    onPressed: onPressed,
+    icon: icon,
+    label: Text(title),
+    style: ButtonStyle(
+        fixedSize: MaterialStateProperty.all(Size(screenSize.width / 2, 20))),
+  );
 }
